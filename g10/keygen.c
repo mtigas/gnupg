09063e24ca34d6ac98b98d76a2c61bba5e1f3528
Mike Tigas <mike@tig.as>
2013-08-09 14:24:33 -0400

    testing incredibly large max key sizes

--- g10/keygen.c
+++ g10/keygen.c
@@ -1981,7 +1981,7 @@ ask_algo (ctrl_t ctrl, int addmode, int *r_subkey_algo, unsigned int *r_usage,
 static unsigned
 ask_keysize (int algo, unsigned int primary_keysize)
 {
-  unsigned int nbits, min, def = DEFAULT_STD_KEYSIZE, max=4096;
+  unsigned int nbits, min, def = DEFAULT_STD_KEYSIZE, max=16384;
   int for_subkey = !!primary_keysize;
   int autocomp = 0;
 
